1dedcef61082817b528221d2edbfb65915a3166b
OrionCync <OrionCync@users.noreply.github.com>
2020-06-24 19:11:33 +0100

    before upload to git

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  var fixHeight = function() {
+    $('.navbar-nav').css(
+      'max-height',
+      document.documentElement.clientHeight - 150
+    );
+  };
+  fixHeight();
+  $(window).resize(function() {
+    fixHeight();
+  });
+  $('.navbar .navbar-toggler').on('click', function() {
+    fixHeight();
+  });
+  $('.navbar-toggler, .overlay').on('click', function() {
+    $('.mobileMenu, .overlay').toggleClass('open');
+  });
+});
+$(window).scroll(function() {
+  $('nav').toggleClass('.scrolled', $(this).scrollTop() > 100);
+});
+$('.navbar-nav>li>a').on('click', function() {
+  $('.navbar-collapse').collapse('hide');
+});
